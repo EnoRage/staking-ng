babf7b948ef55d5a583c66a612a1d189b6ec3470
artem <artall64@gmail.com>
2019-08-29 00:33:47 +0300

    cleanup

--- src/app/pos-delegators/pos-delegators.component.ts
+++ src/app/pos-delegators/pos-delegators.component.ts
@@ -1,46 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {HttpClient} from "@angular/common/http";
-import {CosmosService, CosmosServiceInstance, Validator} from "../cosmos.service";
-import {Observable} from "rxjs";
-import {map, take} from "rxjs/operators";
+import {HttpClient} from '@angular/common/http';
+import {CosmosService, CosmosServiceInstance, Validator} from '../cosmos.service';
+import {Observable} from 'rxjs';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-pos-delegators',
   templateUrl: './pos-delegators.component.html',
   styleUrls: ['./pos-delegators.component.scss']
 })
-export class PosDelegatorsComponent implements OnInit {
+export class PosDelegatorsComponent {
 
-  blockchain : string;
-  // @ts-ignore
-  validators : Array<Validator> = [];
+  blockchain: string;
+  validators: Array<Validator> = [];
   cosmosInstance: CosmosServiceInstance;
 
-  constructor( activatedRoute : ActivatedRoute, private http : HttpClient, private cosmos : CosmosService, private router : Router, ) {
+  constructor(activatedRoute: ActivatedRoute, private http: HttpClient, private cosmos: CosmosService, private router: Router,) {
     this.blockchain = activatedRoute.snapshot.params.blockchainId;
     this.cosmosInstance = this.cosmos.getInstance('cosmos1cj7u0wpe45j0udnsy306sna7peah054upxtkzk');
-    // @ts-ignore
-    this.getValidators().subscribe(( validators : Array<Validator> ) => {
-      this.validators = validators;
-    })
-  }
 
-  ngOnInit() {
+    this.getValidators().subscribe((validators: Array<Validator>) => {
+      this.validators = validators;
+    });
   }
 
   // @ts-ignore
-  getValidators() : Observable<Array<Validator>> {
+  getValidators(): Observable<Array<Validator>> {
     // @ts-ignore
     return this.cosmosInstance.getValidators().pipe(
-      map(( x ) => {
+      map((x) => {
         // @ts-ignore
         return x.docs;
       }),
       take(1)
     );
   }
-  navigateToMyStakeHoldersList( item : Validator ) {
+
+  navigateToMyStakeHoldersList(item: Validator) {
     this.router.navigate([`/details/${item.id}`]);
   }
 
