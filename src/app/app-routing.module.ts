d97c8104c62e3b61ac7e3a8bbca9e65d45bb628d
Nick Kozlov <nk@buttonwallet.com>
2019-08-28 05:01:10 +0300

    Staking works now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AppComponent} from "./app.component";
 import {MainComponent} from "./main/main.component";
 
 const routes: Routes = [
@@ -12,7 +11,8 @@ const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
-  },
+  }
+
 ];
 
 @NgModule({
